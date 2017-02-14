793ee63f1249261b6096600d00363b09b686482f
QianFan Jiang <bucherjiang@msn.com>
2017-02-14 11:01:09 +0800

    adjust project layout with netstandard library

--- source/UWPTest/MainPage.xaml.cs
+++ source/UWPTest/MainPage.xaml.cs
@@ -1,5 +1,17 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -13,13 +25,6 @@ namespace UWPTest
         public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Chakra.NET.ChakraRuntime runtime = Chakra.NET.ChakraRuntime.Create();
-            var context = runtime.CreateContext(true);
-            string s = context.RunScript("echo('333');");
         }
     }
 }
--- source/UWPTest/Properties/AssemblyInfo.cs
+++ source/UWPTest/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Chakra.NET")]
+[assembly: AssemblyTitle("UWPTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chakra.NET")]
+[assembly: AssemblyProduct("UWPTest")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
