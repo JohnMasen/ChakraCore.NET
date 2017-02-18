74a1abbe9a3ebadc7ba4f7b424f2c81bfb35589f
QianFan Jiang <bucherjiang@msn.com>
2017-02-18 17:42:59 +0800

    add sharedmemorybuffer

---
+++ source/ChakraCore.NET/SharedMemory/SharedMemoryBuffer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ChakraCore.NET.SharedMemory
+{
+    public class SharedMemoryBuffer : SafeBuffer
+    {
+        public IntPtr DataAddress  =>  handle;
+        public SharedMemoryBuffer(int size):base(true)
+        {
+            var h=Marshal.AllocHGlobal(size);
+            SetHandle(h);
+            Initialize((ulong)size);
+        }
+        public SharedMemoryBuffer(IntPtr data,int size) : base(false)
+        {
+            SetHandle(data);
+            Initialize((ulong)size);
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            Marshal.FreeHGlobal(handle);
+            return true;
+        }
+    }
+}
