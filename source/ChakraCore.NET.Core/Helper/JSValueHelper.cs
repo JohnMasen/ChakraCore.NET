dae19298bb66ff6ff13736315e151c64133e759c
QianFan Jiang <bucherjiang@msn.com>
2017-03-03 15:06:00 +0800

    add extension method to support default value while reading properties

---
+++ source/ChakraCore.NET.Core/Helper/JSValueHelper.cs
@@ -0,0 +1,34 @@
+﻿using ChakraCore.NET.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChakraCore.NET
+{
+    public static class JSValueHelper
+    {
+        public static T ReadProperty<T>(this IJSValueService service,JavaScriptValue target, JavaScriptPropertyId id,T defaultValue)
+        {
+            if (service.HasProperty(target,id))
+            {
+                return service.ReadProperty<T>(target, id);
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        public static T ReadProperty<T>(this IJSValueService service,JavaScriptValue target,string id,T defaultValue)
+        {
+            if (service.HasProperty(target, id))
+            {
+                return service.ReadProperty<T>(target, id);
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
