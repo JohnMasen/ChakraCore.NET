7af7fdf00b62bc9b8207e5ceaf9f15bf464b306c
QianFan Jiang <bucherjiang@msn.com>
2017-10-28 18:31:33 +0800

    add cache to require feature

--- source/ChakraCore.NET/JSRequireLoader.cs
+++ source/ChakraCore.NET/JSRequireLoader.cs
@@ -7,20 +7,15 @@ namespace ChakraCore.NET
     public class JSRequireLoader
     {
         public string RootPath { get; set; } = string.Empty;
+        private Dictionary<string, string> scriptCache = new Dictionary<string, string>();
         public string LoadLib(string name)
         {
-            System.IO.DirectoryInfo info = new System.IO.DirectoryInfo(System.IO.Directory.GetCurrentDirectory()+"\\"+ RootPath);
+            if (!scriptCache.ContainsKey(name))
+            {
+                scriptCache.Add(name, load(name));
+            }
+            return scriptCache[name];
             
-            string fileName = name + ".js";
-            var files = info.GetFiles(fileName);
-            if (files.Length==1)
-            {
-                return files[0].OpenText().ReadToEnd();
-            }
-            else
-            {
-                throw new System.IO.FileNotFoundException("cannot find rquired js file", fileName);
-            }
         }
 
         public static void EnableRequire(ChakraContext context,string rootPath=null)
@@ -28,6 +23,22 @@ namespace ChakraCore.NET
             JSRequireLoader loader = new JSRequireLoader() { RootPath = rootPath };
             context.GlobalObject.Binding.SetFunction<string, string>("_loadLib", loader.LoadLib);
             context.RunScript(Properties.Resources.ResourceManager.GetString("JSRequire"));
+        }
+
+        private string load(string name)
+        {
+            System.IO.DirectoryInfo info = new System.IO.DirectoryInfo(System.IO.Directory.GetCurrentDirectory() + "\\" + RootPath);
+
+            string fileName = name + ".js";
+            var files = info.GetFiles(fileName);
+            if (files.Length == 1)
+            {
+                return files[0].OpenText().ReadToEnd();
+            }
+            else
+            {
+                throw new System.IO.FileNotFoundException("cannot find rquired js file", fileName);
+            }
         }
     }
 }
