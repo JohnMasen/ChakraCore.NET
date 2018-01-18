fb3dd7873331b09a7097e209f0e071a95226797a
QianFan Jiang <bucherjiang@msn.com>
2018-01-18 20:29:25 +0800

    improve demo script

--- source/RunScript/Scripts/Hosting/App.js
+++ source/RunScript/Scripts/Hosting/App.js
@@ -1,9 +1,9 @@
-﻿import { echo } from 'echo.js';
+﻿import { echo } from 'echo';
 import * as info from 'info.js';
 import { CreateHosting } from 'Hosting.js';
 export class app {
     main() {
-        echo("start at thread:" + info.GetCurrentThread());
+        echo("[" + info.GetCurrentThread()+"]"+"start");
         CreateHosting("Module1", "Test").then(target => {
             for (let i = 0; i < 10; i++) {
             this.test1(target,i);
@@ -14,7 +14,7 @@ export class app {
     test1(remoteModule,value) {
         remoteModule.Call("Hello", value,value+1)
             .then(result => {
-                echo("Result=" + JSON.stringify(result));
+                echo("[" + info.GetCurrentThread() + "]" +"Result=" + JSON.stringify(result));
                 });
     }
 }
--- source/RunScript/Scripts/Hosting/Module1.js
+++ source/RunScript/Scripts/Hosting/Module1.js
@@ -3,7 +3,7 @@ import * as info from 'info.js';
 
 export class Test  {
     Hello(value1,value2) {
-        echo("Hello run hosted on thread " + info.GetCurrentThread()+ ", value1="+String(value1)+",value2="+String(value2));
+        echo("[" + info.GetCurrentThread() + "]" +"Hello run hosted, value1="+String(value1)+",value2="+String(value2));
         return {
             tag1: "This is object result",
             message: "This is message",
